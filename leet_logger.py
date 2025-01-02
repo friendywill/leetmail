dff3cc8853e990970f7ca48849c736f007045232
friendywill <will@friendy.dev>
2025-01-02 15:26:05 +1100

    chore: concise the format for loggers

--- leet_logger.py
+++ leet_logger.py
@@ -1,7 +1,12 @@
 import logging
 
+# Base format for all logs
+base_format = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+)
+
 # Configure logger for runs-specific logs
-logger_runs = logging.getLogger("leetmail_runs")
+logger_runs = logging.getLogger("LEETMAIL_RUNS")
 logger_runs.setLevel(logging.DEBUG)
 
 # Console handler for logger_runs
@@ -17,24 +22,16 @@ logger_runs.addHandler(runs_console_handler)
 logger_runs.addHandler(runs_file_handler)
 
 # Configure logger for main application logs
-logger_main = logging.getLogger("leetmail")
+logger_main = logging.getLogger("LEETMAIL_APPLICATION")
 logger_main.setLevel(logging.DEBUG)
 
 # Console handler for logger_main
 main_console_handler = logging.StreamHandler()
-main_console_handler.setLevel(logging.DEBUG)
-main_console_format = logging.Formatter(
-    "%(asctime)s - MAIN - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
-)
-main_console_handler.setFormatter(main_console_format)
+main_console_handler.setFormatter(base_format)
 
 # File handler for logger_main
 main_file_handler = logging.FileHandler("leetmail.log")
-main_file_handler.setLevel(logging.DEBUG)
-main_file_format = logging.Formatter(
-    "%(asctime)s - MAIN - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
-)
-main_file_handler.setFormatter(main_file_format)
+main_file_handler.setFormatter(base_format)
 
 # Add handlers to logger_main
 logger_main.addHandler(main_console_handler)
